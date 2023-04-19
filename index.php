fbed235fd2a1d4bf68c1da936c4685b6309bc5d6
Amilton-Melo <amiltonmelo.dev@gmail.com>
2023-04-19 08:33:35 -0300

    Teste com hospedagem

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         <div class="container">
             <div class="menu">
                 <ul>
-                    <li><a href="index.html">Inicio</a></li>
+                    <li><a href="index.php">Inicio</a></li>
                     <li><a href="listar.php">Lista de alunos</a></li>
                     <li><a href="cadastrar.php">Cadastrar aluno</a></li>
                 </ul>
